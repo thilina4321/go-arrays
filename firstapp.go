6a0a2f83ffa92fe1d7fb497e5e59b3d73bdc38dd
thilina_dilshan <thilina.dilshan@arimaclanka.com>
2021-09-11 11:03:42 +0530

    changes

--- firstapp.go
+++ firstapp.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 	"strconv"
 	"strings"
 )
 
-var reader = bufio.NewReader(os.Stdin)
+// var reader = bufio.NewReader(os.Stdin)
 
 // go mod init github.com/myorg/firstapp
 func main() {
